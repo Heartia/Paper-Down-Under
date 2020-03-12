59f042a7b47ee26559bd671702c6340ea2fd3f16
Hollosou <Patnaterson2048@gmail.com>
2020-03-12 16:35:13 -0500

    Update Animation.cs

--- Animation.cs
+++ Animation.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Xna.Framework;
@@ -13,14 +13,48 @@ namespace Paper_Down_Under
 {
     class Animation
     {
+        private Texture2D spritesheet;
+        private Rectangle partOfSpritesheet;
+        private GraphicsDevice graphicsDevice;
+        private int[] imgTicks;
+        private int ticks;
+        private int index;
+
+        public Animation(Texture2D _spritesheet, GraphicsDevice _graphicsDevice, Rectangle _partOfSpritesheet, int[] _imgTicks)
+        {
+            spritesheet = _spritesheet;
+            graphicsDevice = _graphicsDevice;
+            partOfSpritesheet = _partOfSpritesheet;
+            imgTicks = _imgTicks;
+            ticks = 0;
+        }
+
         public Texture2D getFrame()
         {
-            throw new NotSupportedException("Function is not supported in this version");
+            // Create a new texture of the desired size
+            Texture2D croppedTexture = new Texture2D(graphicsDevice, partOfSpritesheet.Width, partOfSpritesheet.Height);
+
+            // Copy the data from the cropped region into a buffer, then into the new texture
+            Color[] data = new Color[partOfSpritesheet.Width * partOfSpritesheet.Height];
+            spritesheet.GetData(0, partOfSpritesheet, data, 0, partOfSpritesheet.Width * partOfSpritesheet.Height);
+            croppedTexture.SetData(data);
+
+            return croppedTexture;
         }
 
         public void next()
         {
-            throw new NotSupportedException("Function is not supported in this version");
+            ticks++;
+            if (ticks >= imgTicks[index])
+            {
+                ticks = 0;
+                index++;
+                if (index >= imgTicks.Length)
+                {
+                    index = 0;
+                }
+                partOfSpritesheet = new Rectangle(partOfSpritesheet.Width * index, partOfSpritesheet.Y, partOfSpritesheet.Width, partOfSpritesheet.Height);
+            }
         }
     }
 }
