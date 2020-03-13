dedd5c38ec65f34b7c780f816946b6f3a7aef12d
ericdai1 <61884535+ericdai1@users.noreply.github.com>
2020-03-13 17:23:03 -0500

    Add files via upload

--- Enemy.cs
+++ Enemy.cs
@@ -9,52 +9,97 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 
-namespace Game_v1
+namespace Paper_Down_Under
 {
 
     class Enemy : GameObject
     {
-        Texture2D texture;
-        Rectangle[] anims;
-        int anim = 0;
+        private int hP;
+        private int def;
+        private int acc;
+        private int atk;
+        private string status;
+        private string name;
+        private int gold;
+
         float timer = 0;
-        int sourceX;
-        int sourceY;
+
 
         //sX = pixels horizontally for each sprite in the sheet
         //sY = height of spritesheet
         //count = # of images in spritesheet
-        public Enemy(Texture2D t, int sX, int sY, int count, Animation _idleAnim, Rectangle _loc, Camera _camera) : base(_idleAnim, _loc, _camera)
+        public Enemy(int h, int d, int ac, int at, int g, string n, Animation _idleAnim, Rectangle _loc, Camera _camera) : base(_idleAnim, _loc, _camera)
         {
-            texture = t;
-            sourceX = sX;
-            sourceY = sY;
-            anims = new Rectangle[count];
-            loc = _loc;
-            Initialize();
+            hP = h;
+            def = d;
+            acc = ac;
+            atk = at;
+            name = n;
+            gold = g;
         }
-        public void Initialize()
+        public int getHP()
         {
-            for (int i = 0; i < anims.Count(); i++)
+            return hP;
+        }
+        public void setHP(int newHP)
+        {
+            hP = newHP;
+        }
+        public int getDef()
+        {
+            return def;
+        }
+        public void setDef(int d)
+        {
+            def = d;
+        }
+        public int getAcc()
+        {
+            return acc;
+        }
+        public void setAcc(int ac)
+        {
+            acc = ac;
+        }
+        public int getAtk()
+        {
+            return atk;
+        }
+        public void setAtk(int at)
+        {
+            atk = at;
+        }
+        public int getGold()
+        {
+            return gold;
+        }
+        //poison, burn, etc.
+        public void setStatus(string status, int dmg, int turns)
+        {
+
+        }
+        //when player attacks the enemy
+        public int damaged(int dmg)
+        {
+            if (dmg <= def)
+                return 0;
+            setHP(hP - (dmg - def));
+            return (dmg - def);
+        }
+        //Below comes after Player Class is made
+        //public string offense(int dmg, Player player)
+        //{
+        //    int attack = player.damaged(dmg);
+        //    string ret = name + " dealt " + attack + " damage to you!";
+
+        //}
+        public Boolean isAlive()
+        {
+            if (hP <= 0)
             {
-                anims[i] = new Rectangle(sourceX * i, 0, sourceX, sourceY);
+                return false;
             }
-        }
-        public void Update(GameTime gameTime)
-        {
-            timer++;
-            if (timer % 2 == 0)
-            {
-                anim++;
-                if (anim > 5)
-                {
-                    anim = 0;
-                }
-            }
-        }
-        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
-        {
-            spriteBatch.Draw(texture, loc, anims[anim], Color.White);
+            return true;
         }
     }
 }
